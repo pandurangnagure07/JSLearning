b12681867e62de9a892e995ac0e210f9bd939faa
pandurangnagure07 <pandurangnagure07@gmail.com>
2023-02-16 22:32:58 +0530

    01_Assignment
    JSAssign

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,23 @@
- console.log (" Pandurang Ngaure ");
+ // Personal Details
+ console.log (" Pandurang Ngaure  ");
+console.log(23);
+console.log("Satara");
+
+var fullName = "Pandurang Nagure";
+console.log(fullName);
+console.log(fullName);
+
+var city;              // Variable Decleration
+console.log(city);
+city = "Pune";        // variable initialization
+console.log(city);
+city = "Satara";      //Variable Update
+console.log(city); 
+
+var age = "23";      //Variable decleration and initialization in same line
+age = "24";
+age = "25";
+console.log(age);
+
+var university = "Shivaji University Kolhapur"; // variable initialization
+console.log(university);
---
+++ variableAssignment_one.js
@@ -0,0 +1,16 @@
+console.log("\n**********STEP1**********");
+console.log("I Love Only JavaScript");
+
+console.log("\n**********STEP2**********");
+console.log("My Full Name Is: ");
+console.log("                  Pandurang Maruti Nagure");
+
+console.log("\n**********STEP3**********");
+var firstName = "Pandurang,";
+var lastName = "Nagure,";
+var age = "23,";
+var address = "Satara,";
+var pincode = "415002,";
+var gender = "Male,";
+var isMarried ="Single Need Someone."
+console.log(firstName,lastName,age,address,pincode,gender,isMarried); 
