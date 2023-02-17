128a97d06a21bd5cd60658fe11780d2b5c1af30b
pandurangnagure07 <pandurangnagure07@gmail.com>
2023-02-17 08:59:39 +0530

    02
    datatypes completed

---
+++ 02_datatypes.js
@@ -0,0 +1,29 @@
+var myName = 'Pandurang Nagure';
+
+var age = 23;
+var weight = 56;
+
+var isMarried = true;
+var isLazy = false;
+
+var typeOfmyName = typeof myName;
+console.log("Type of variable Name: ", typeOfmyName);
+var typeOffatherName = typeof fatherName;
+console.log("type of variable:", typeOffatherName);
+
+console.log("Type of variable age:", age);
+console.log("Type of variable weight:", weight);
+
+console.log("Type of variable isMarried:", isMarried);
+console.log("Type of variable isLazy:", isLazy);
+
+var graduation;
+console.log("Value of graduation variable is: ", graduation);
+console.log("typeof graduation is: ", typeof graduation);
+
+var city = null;
+console.log("Value of city variable is: ", city);
+console.log("type of city is: ", typeof city);
+
+age = 100; 
+
