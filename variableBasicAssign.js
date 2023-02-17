896d3ca551cc23550103f951062cf689b67e56db
pandurangnagure07 <pandurangnagure07@gmail.com>
2023-02-17 22:09:57 +0530

    Completed assign
    Completed Variablebasic assignment

---
+++ variableBasicAssign.js
@@ -0,0 +1,24 @@
+console.log("*****STEP1*****");
+var pincode = 41502;
+console.log("Value of pincode variable is : ",pincode);
+console.log("Type of Pincode is : " ,typeof pincode);
+
+console.log("\n*****STEP2*****");
+var name = "Pandurang Nagure";
+console.log("Value of name variable is : ",name);
+console.log("Type of name is : " ,typeof name);
+
+console.log("\n*****STEP3*****");
+var married = false ;
+console.log("Value of isMarried variable is : ",married);
+console.log("Type of isMarried is : " ,typeof married);
+
+console.log("\n*****Difference*****");
+console.log("\nNULL : \n 1.It is an assignment value. It can be assigned to a variable which indicates that a variable does not point any object.");
+console.log(" 2.The null value is a primitive value which represents the null, empty, or non-existent reference.")
+console.log(" 3.Null indicates the absence of a value for a variable.")
+
+console.log("\nUndefined:");
+console.log("1.It is not an assignment value. It means a variable has been declared but has not yet been assigned a value.");
+console.log("2.The undefined value is a primitive value, which is used when a variable has not been assigned a value.");
+console.log("Undefined indicates the absence of the variable itself.");
