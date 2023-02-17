ef8d5b4ad0096471af1ea23ce1fa5b90777c7654
pandurangnagure07 <pandurangnagure07@gmail.com>
2023-02-17 22:35:46 +0530

    Completed 02_typeof.js
    02_typeOf.js

---
+++ typeOfAssign.js
@@ -0,0 +1,22 @@
+var bankName;
+
+console.log(bankName);
+bankName = "SBI Bank";
+
+console.log(bankName);
+console.log("Data Type Of BankName is : ",typeof bankName);
+
+marks= 90 ;
+console.log(marks);
+console.log("Data Type Of marks is : ", typeof marks);
+
+var isWorking;
+console.log(isWorking);
+isWorking = false;
+console.log(isWorking);
+console.log("Type of isWorking ia : ",typeof isWorking);
+
+var totalCount= "Hunderd and Seven";
+console.log(totalCount, typeof totalCount);
+totalCount = 107;
+console.log(totalCount, typeof totalCount);
