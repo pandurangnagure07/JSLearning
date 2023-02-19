bdeaa2ad61ed02053bb3b75a12fb306b6f51cabe
pandurangnagure07 <pandurangnagure07@gmail.com>
2023-02-19 14:16:28 +0530

    completed
    Completed TWO and THREE variables swapping

---
+++ swapVariables_02.js
@@ -0,0 +1,34 @@
+console.log("Swapping Variables");
+var num1 = "sweety";
+var num2 = "cutie";
+console.log("\n*****Before Swaping*****")
+num1=num2;
+console.log(num1,num2);
+
+console.log("\n*****After Swaping*****")
+var num1 = "sweety";
+var num2 = "cutie";
+var temp = num1 ;
+num1 = num2;
+num2 = temp;
+console.log(num1,num2);
+
+console.log("\n*****Three Variables Swaping*****")
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+console.log("\n*****Before Swapping*****")
+console.log(num1);
+console.log(num2);
+console.log(num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+
+
+console.log("\n*****After Swapping*****")
+console.log(num1);
+console.log(num2);
+console.log(num3);
